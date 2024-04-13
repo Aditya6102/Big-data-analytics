189e0c2ebb9288e6fb324b16e772defd921839d0
Aditya6102 <166187172+Aditya6102@users.noreply.github.com>
2024-04-13 00:00:05 -0400

    Add files via upload

---
+++ ASSIGNMENT_1.R
@@ -0,0 +1,28 @@
+#Question1
+A<-7
+B<-12
+C<-5
+D<-(A+B+C)/3
+print(paste("The Average is:",D))
+
+#Question2
+Firstname<-"Sai"
+print(paste("Welcome to R,",Firstname))
+
+#Question3
+X<-7
+Y<-12
+Z<-4
+
+if(X>Y){
+  print("X is bigger than Y")
+} else(
+  print("X is smaller than Y")
+)
+
+
+if(X>Z){
+  print("X is bigger than Z")
+}else(
+  print("X is smaller than Z")
+)
